1acedd23adec71ca4f90907ca7aa1ac0f211f3dd
bdziadosz96 <bdziadosz96@icloud.com>
2022-01-27 22:27:39 +0100

    Cart counting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {CatalogService} from "./catalog.service";
 import {Book} from "./book";
+import {CartItem} from "./CartItem";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'frontebookstore';
   public catalog: Book[] | undefined;
+  public cartItems: CartItem[] = [];
 
 
-  constructor(private catalogService: CatalogService) {}
+  constructor(private catalogService: CatalogService) {
+  }
 
   ngOnInit(): void {
     this.getCatalog();
@@ -25,5 +28,21 @@ export class AppComponent implements OnInit{
         console.log(response);
         this.catalog = response;
       });
+  }
+
+  public countCartItems(): number {
+    return this.cartItems
+      .reduce((sum, current) => sum + current.quantity, 0);
+  }
+
+  public addToCart(book: Book): void {
+    // @ts-ignore
+    const current: CartItem = this.cartItems.find(x => x.bookId == book.id);
+    if (current) {
+      current.quantity += 1;
+    } else {
+      this.cartItems.push({bookId: book.id, quantity: 1} as CartItem)
+    }
+    console.log('Cart is: ' + JSON.stringify(this.cartItems));
   }
 }
